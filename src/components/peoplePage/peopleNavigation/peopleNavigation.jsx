37178abfb9f852d64761472327dc5602f5ec0611
DakitaNorth <garryant3120@yandex.ru>
2022-08-04 17:49:50 +0300

    Commit #10
    Кнопки компонента "Персонажи" вынесены в отдельный UI-компонент
    Развернута библиотека StoryBook

--- src/components/peoplePage/peopleNavigation/peopleNavigation.jsx
+++ src/components/peoplePage/peopleNavigation/peopleNavigation.jsx
@@ -1,19 +1,30 @@
 import { Link } from "react-router-dom";
 
+import UiButton from "@UI/uiButton/uiButton";
+
 import styles from "./peopleNavigation.module.css";
 
 const PeopleNavigation = ({ getResource, counterPage, prevPage, nextPage }) => {
 
-    const handleChangeNext = () => getResource(nextPage);
     const handleChangePrev = () => getResource(prevPage);
+    const handleChangeNext = () => getResource(nextPage);
 
     return (
-        <div>
-            <Link to={`/people/?page=${counterPage - 1}`} className={styles.link}>
-                <button onClick={handleChangePrev} disabled={!prevPage} className={styles.buttons}>Previous</button>
+        <div className={styles.container}>
+            <Link to={`/people/?page=${counterPage - 1}`} className={styles.buttons}>
+                <UiButton
+                    text="Previous"
+                    onClick={handleChangePrev}
+                    disabled={!prevPage}
+                    classes
+                />
             </Link>
-            <Link to={`/people/?page=${counterPage + 1}`} className={styles.link}>
-                <button onClick={handleChangeNext} disabled={!nextPage} className={styles.buttons}>Next</button>
+            <Link to={`/people/?page=${counterPage + 1}`} className={styles.buttons}>
+                <UiButton
+                    text="Next"
+                    onClick={handleChangeNext}
+                    disabled={!nextPage}
+                />
             </Link>
         </div>
     );
